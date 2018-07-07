d78a1481dfd5a14e14f02bc709a5df4908f48f18
Pavel Ray <pavel.ray@lowes.com>
2018-07-07 21:50:14 +0530

    Updated  application.py

--- app/application.py
+++ app/application.py
@@ -9,18 +9,17 @@ from resources.location import Location
 from resources.item import Item
   
 
-if __name__ == '__main__':
-    from utility.database import db
-    application = create_app()
-    api = Api(application)
-    jwt = JWT(application,authenticate,identity)
+application = create_app()
+api = Api(application)
+jwt = JWT(application,authenticate,identity)
     
     
-    api.add_resource(LostAndFound,'/lost_found/')
-    api.add_resource(Lost,'/lost/')
-    api.add_resource(Found,'/found/')
-    api.add_resource(LostAndFoundByKey,'/lost_found/<string:search_key>')
-    api.add_resource(Location,'/location/')
-    api.add_resource(Item,'/items/')
-    
+api.add_resource(LostAndFound,'/lost_found/')
+api.add_resource(Lost,'/lost/')
+api.add_resource(Found,'/found/')
+api.add_resource(LostAndFoundByKey,'/lost_found/<string:search_key>')
+api.add_resource(Location,'/location/')
+api.add_resource(Item,'/items/')
+
+if __name__ == '__main__':    
     application.run(debug=True)
--- app/utility/config.py
+++ app/utility/config.py
@@ -1,5 +1,4 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "mysql://root:root1234@mytestdb.cvkjenxs1qov.us-east-1.rds.amazonaws.com/mytestdb"
     SQLALCHEMY_DATABASE_LOCAL = "mysql://root:root@localhost:3306/TjXtqyVLz2"
     DEBUG = True
     SQLALCHEMY_ECHO = True
