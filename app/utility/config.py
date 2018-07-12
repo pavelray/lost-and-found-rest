7281659c179d23704719fcb3978834020ed215c9
Pavel Ray <pavel.ray@lowes.com>
2018-07-12 17:09:29 +0530

    added gitignore

--- app/utility/config.py
+++ app/utility/config.py
@@ -1,5 +1,5 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "mysql://wesmartappers:wecandoit12345@wesmartappers.ctiyfazonxhr.us-east-1.rds.amazonaws.com/LostAndFoundGlobal"
+    
     SQLALCHEMY_DATABASE_LOCAL = "mysql://root:root@localhost:3306/TjXtqyVLz2"
     DEBUG = True
     SQLALCHEMY_ECHO = True
